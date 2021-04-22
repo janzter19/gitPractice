99ab725ba716a37910982254ce9a9246b77a97db
Janzter <janzter@gmail.com>
2021-04-23 00:51:25 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,6 @@ Update Test #1
 <br />
 Update Test #2
 <br />
-<<<<<<< HEAD
-Update Test #3
 
-bien pogi
 
-janzter 1234
-=======
-Update Test #3
->>>>>>> parent of 08c227c (bien pogi)
+EDIT SA BRANCH
